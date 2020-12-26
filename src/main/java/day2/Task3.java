e446d76b5fa7ba4c36a72a06566f7208141e7231
k00lith <Noipt1589>
2020-12-27 01:25:25 +0300

    add solutions for day2 tasks

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -7,17 +7,17 @@ public class Task3 {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-//        if (a < b) {
-//            int count = 0;
-//            int rsl = b - a;
-//            while (count < rsl) {
-//                count++;
-//                if (((count + a) % 5 == 0) && ((count + a) % 10 != 0)) {
-//                    System.out.print(count + " ");
-//                }
-//            }
-//        } else {
-//            System.out.println("Некорректный ввод");
-//        }
+        if (a < b) {
+            int count = 0;
+            int rsl = b - a;
+            while (count < rsl) {
+                count++;
+                if (((count + a) % 5 == 0) && ((count + a) % 10 != 0)) {
+                    System.out.print((count + a) + " ");
+                }
+            }
+        } else {
+            System.out.println("Некорректный ввод");
+        }
     }
 }
