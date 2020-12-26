87f43eb0cc4b857424fccddc856ec42dde920e33
k00lith <Noipt1589>
2020-12-27 00:58:38 +0300

    pre_commit day2

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,24 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        System.out.println("run");
+        Scanner scan = new Scanner(System.in);
+        int inInfo = scan.nextInt();
+        if (inInfo <= 0) {
+            System.out.println("Ошибка ввода");
+        }
+        if ((inInfo >= 1) && (inInfo <= 4)) {
+            System.out.println("Малоэтажный дом");
+        }
+        if ((inInfo >= 5) && (inInfo <= 8)) {
+            System.out.println("Среднеэтажный дом");
+        }
+        if (inInfo >= 9) {
+            System.out.println("Многоэтажный дом");
+        }
 
     }
 }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,23 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+//        if (a < b) {
+//            int count = 0;
+//            int rsl = b - a;
+//            while (count < rsl) {
+//                count++;
+//                if (((count + a) % 5 == 0) && ((count + a) % 10 != 0)) {
+//                    System.out.print(count + " ");
+//                }
+//            }
+//        } else {
+//            System.out.println("Некорректный ввод");
+//        }
     }
 }
