1527a43a1e29ab206dd4f8aca092ee23ada8215e
k00lith <Noipt1589>
2020-12-28 23:25:00 +0300

    add solutions for day3 tasks

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -1,7 +1,84 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String city = scanner.nextLine();
+        switch (city) {
+            case "Москва":
+            case "Владивосток":
+            case "Ростов":
+                System.out.println("Россия");
+                break;
+            case "Рим":
+            case "Милан":
+            case "Турин":
+                System.out.println("Италия");
+                break;
+            case "Ливерпуль":
+            case "Манчестер":
+            case "Лондон":
+                System.out.println("Англия");
+                break;
+            case "Берлин":
+            case "Мюнхен":
+            case "Кёльн":
+                System.out.println("Германия");
+                break;
+            case "Stop":
+                System.out.println("*программа завершила работу*");
+                break;
+            default:
+                System.out.println("Неизвестная страна");
+        }
     }
 }
+
+
+/**
+ *
+ * package day3;
+ *
+ * import java.util.Scanner;
+ *
+ * public class Task1 {
+ *     public static void main(String[] args) {
+ *         Scanner scanner = new Scanner(System.in);
+ *         boolean rsl = true;
+ *         while (rsl) {
+ *             System.out.println("Введите город: ");
+ *             String city = scanner.nextLine();
+ *             switch (city) {
+ *                 case "Москва":
+ *                 case "Владивосток":
+ *                 case "Ростов":
+ *                     System.out.println("Россия");
+ *                     break;
+ *                 case "Рим":
+ *                 case "Милан":
+ *                 case "Турин":
+ *                     System.out.println("Италия");
+ *                     break;
+ *                 case "Ливерпуль":
+ *                 case "Манчестер":
+ *                 case "Лондон":
+ *                     System.out.println("Англия");
+ *                     break;
+ *                 case "Берлин":
+ *                 case "Мюнхен":
+ *                 case "Кёльн":
+ *                     System.out.println("Германия");
+ *                     break;
+ *                 case "Stop":
+ *                     rsl = false;
+ *                     System.out.println("*программа завершила работу*");
+ *                     break;
+ *                 default:
+ *                     System.out.println("Неизвестная страна");
+ *             }
+ *         }
+ *     }
+ * }
+ */
--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        boolean rsl = true;
+        while (rsl) {
+            double a = scanner.nextDouble(); // делимое
+            double b = scanner.nextDouble(); // делитель
+            if (b != 0) {
+                System.out.println(a / b);
+            } else {
+                rsl = false;
+                break;
+            }
+        }
     }
 }
