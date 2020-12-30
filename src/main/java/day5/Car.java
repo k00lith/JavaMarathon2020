dbcff45d1176efc8c0ebdad189e6335bbba8ef01
k00lith <Noipt1589>
2020-12-30 17:07:35 +0300

    День 5. Вынес классы в разные файлы java

---
+++ src/main/java/day5/Car.java
@@ -0,0 +1,31 @@
+package day5;
+
+class Car {
+    private int age;
+    private String color;
+    private String model;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}
--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -10,27 +10,3 @@ public class Task1 {
 
     }
 }
-class Car {
-    private int age;
-    private String color;
-    private String model;
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-    public void setColor(String color) {
-        this.color = color;
-    }
-    public void setModel(String model) {
-        this.model = model;
-    }
-    public int getAge() {
-        return age;
-    }
-    public String getColor() {
-        return color;
-    }
-    public String getModel() {
-        return model;
-    }
-}
