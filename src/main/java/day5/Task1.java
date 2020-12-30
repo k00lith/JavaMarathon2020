27244d674b9e1274e5bcb68e07dd68dc463787f0
k00lith <Noipt1589>
2020-12-30 17:00:37 +0300

    Добавил решениея задач день 5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,35 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setAge(2015);
+        car.setColor("Blue");
+        car.setModel("Tesla");
+        System.out.println(car.getAge() + "\n\n" + car.getColor() + "\n\n" + car.getModel());
 
+    }
+}
+class Car {
+    private int age;
+    private String color;
+    private String model;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getColor() {
+        return color;
+    }
+    public String getModel() {
+        return model;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,8 +1,29 @@
 package day5;
 
-
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike moto = new Motorbike(2015, "blue", "suzuki");
+        System.out.println(moto.getAge() + "\n\n" + moto.getColor() + "\n\n" + moto.getModel());
 
+    }
+}
+class Motorbike {
+    private int age;
+    private String color;
+    private String model;
+
+    public Motorbike(int age, String color, String model) {
+        this.age = age;
+        this.color = color;
+        this.model = model;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getColor() {
+        return color;
+    }
+    public String getModel() {
+        return model;
     }
 }
