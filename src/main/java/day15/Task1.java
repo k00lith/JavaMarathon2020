2241119ac2e55e725f9cd861768e0ba2532d8bfb
k00lith <koolith@gmail.com>
2021-01-13 00:07:30 +0300

    Добавил решения для заданий - День 15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,23 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File source = new File("src/main/resources/shoes.csv");
+        PrintWriter dest = new PrintWriter("src/main/resources/missing_shoes.txt");
+        Scanner scanner1 = new Scanner(source);
+        while(scanner1.hasNextLine()) {
+            String line = scanner1.nextLine();
+            char zero = '0';
+            char point = ';';
+            if (line.charAt(line.length() - 1) == zero && line.charAt(line.length() - 2) == point) {
+                dest.println(line);
+            }
+        }
+        dest.close();
     }
 }
