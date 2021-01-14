2040fc153604005519846951923106474935a8fa
k00lith <koolith@gmail.com>
2021-01-14 22:39:08 +0300

    Добавил решения для заданий - День 18

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -2,6 +2,14 @@ package day18;
 
 public class Task1 {
     public static void main(String[] args) {
+        int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
+        recursionSum(numbers, 0);
+    }
 
+    public static int recursionSum(int[] n, int i) {
+        if (i == n.length) {
+            return 0;
+        }
+        return n[i] + recursionSum(n, i + 1);
     }
 }
